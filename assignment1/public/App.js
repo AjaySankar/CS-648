8a8d68eeac80f107cc78778832bcf0e13998eec6
Ajay Sankar <ajay0196@gmail.com>
2020-02-02 15:39:55 -0800

    Updated description and headshot
    Updated description and headshot

--- assignment1/public/App.js
+++ assignment1/public/App.js
@@ -3,16 +3,16 @@ var component = React.createElement("div", {
   className: "grid-container"
 }, React.createElement("div", {
   className: "item1"
-}, React.createElement("h1", null, " Batman ")), React.createElement("div", {
+}, React.createElement("h1", null, " Ajay Sankar ")), React.createElement("div", {
   className: "item2"
 }, React.createElement("img", {
-  src: "./img/batman_headshot.jpg",
+  src: "./img/headshot.jpg",
   alt: "My HeadShot",
   width: "200",
   height: "200"
 })), React.createElement("div", {
   className: "item3"
-}, "I have been Gotham's protector for decades, CEO of Wayne Enterprises, Patriarch of the Bat Family and veteran member of the Justice League.Unlike most superheroes, I do not possess any superpowers; I make use (to the best that I can) of intellect, detective skills, science and technology, wealth, physical prowess, and intimidation in his war on crime."), React.createElement("div", {
+}, "A 3 year experienced Senior software engineer with a demonstrated history of working in C++, Java, ES6, NPM, WebPack, Perl and Modern web development, header-bidding and programmatic advertising platform."), React.createElement("div", {
   className: "item4"
 }, React.createElement("a", {
   href: "https://github.com/AjaySankar/",
--- assignment1/src/App.jsx
+++ assignment1/src/App.jsx
@@ -1,13 +1,13 @@
 var contentNode = document.getElementById('root');
 var component = <div className="grid-container">
     <div className="item1">
-      <h1> Batman </h1>
+      <h1> Ajay Sankar </h1>
     </div>
     <div className="item2">
-      <img src= "./img/batman_headshot.jpg" alt="My HeadShot" width="200" height="200"></img>
+      <img src= "./img/headshot.jpg" alt="My HeadShot" width="200" height="200"></img>
     </div>
     <div className="item3">
-      I have been Gotham's protector for decades, CEO of Wayne Enterprises, Patriarch of the Bat Family and veteran member of the Justice League.Unlike most superheroes, I do not possess any superpowers; I make use (to the best that I can) of intellect, detective skills, science and technology, wealth, physical prowess, and intimidation in his war on crime.
+      A 3 year experienced Senior software engineer with a demonstrated history of working in C++, Java, ES6, NPM, WebPack, Perl and Modern web development, header-bidding and programmatic advertising platform.
     </div>
     <div className="item4">
       <a href="https://github.com/AjaySankar/" target = "_blank" className="myButton">VIEW MY GITHUB REPO</a>
